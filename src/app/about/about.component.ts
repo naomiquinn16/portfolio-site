dc414ab46178e66005f5c5d08bcb17532c72621f
Naomi Quinn <naomi.e.quinn@accenture.com>
2019-12-09 00:59:22 +0000

    added logo animation

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,26 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { trigger, state, style, animate, transition } from '@angular/animations';
+
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
   animations: [
-    trigger('fadeInOut', [
-      state('void', style({
+    trigger('fadeIn', [
+      state('void',
+        style({
         opacity: 0
       })),
-      transition('void <=> *', animate(1000)),
-    ]),
+      transition('void <=> *', animate('3s 2s ease-in')),
+    ])
   ]
 })
 export class AboutComponent implements OnInit {
-  currentState = 'initial';
 
   constructor() { }
 
   ngOnInit() {
   }
-  changeState() {
-    this.currentState = this.currentState === 'initial' ? 'final' : 'initial';
-  }
+
 }
